00a59c4a194bf49a9578d1e10fd5e32349ae257c
RAlexander777 <recterritory@gmail.com>
2025-12-12 11:38:53 -0500

    astro config sin www

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // 1. Dominio 
-  site: "https://www.hostalacuariuspuno.com", 
+  site: "https://hostalacuariuspuno.com", 
   
   // 2. Carpeta del repositorio
   base: "/",
